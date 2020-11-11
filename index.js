b3e994197008f61dbfb5635f3b183ffafd0ab4a2
dickymaulanaa <dickymaulanaa@gmail.com>
2020-11-11 09:39:35 +0700

    bulan

---
+++ index.js
@@ -0,0 +1,40 @@
+var date= new Date ('Wed Mar 21 2020 11:44:33')
+var month = date.getMonth()
+console.log(month);
+switch (month){
+    case 0:
+    console.log("januari");
+    break;
+    case 1:
+    console.log("februari");
+    break;
+    case 2:
+    console.log("maret");
+    break;
+    case 3:
+    console.log("april");
+    break;
+    case 4:
+    console.log("mei");
+    break;
+    case 5:
+    console.log("juni");
+    break;
+    case 6:
+    console.log("juli");
+    break;
+    case 7:
+    console.log("agustus");
+    break;
+    case 8:
+    console.log("september");
+    break;
+    case 9:
+    console.log("oktober");
+    break;
+    case 10:
+    console.log("november");
+    break;
+    case 11:
+    console.log("desember");
+}
